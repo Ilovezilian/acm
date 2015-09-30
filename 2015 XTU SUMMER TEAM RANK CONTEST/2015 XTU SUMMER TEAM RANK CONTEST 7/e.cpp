4bb16cbaebd05fd34ce9345846a0ea3d9839601f
Ilovezilian <1084288424@qq.com>
2015-09-30 13:02:24 +0800

    The last time I commit code from the acm lab

---
+++ 2015 XTU SUMMER TEAM RANK CONTEST/2015 XTU SUMMER TEAM RANK CONTEST 7/e.cpp
@@ -0,0 +1,37 @@
+/* ***********************************************
+Author        :Ilovezilian
+Created Time  :2015/9/29 17:53:50
+File Name     :e.cpp
+************************************************ */
+
+#include <bits/stdc++.h>
+#define fi(i,n) for(int i = 0; i < n; i ++)
+#define fin(i,n1,n2) for(int i = n1; i < n2; i ++)
+#define ll long long
+#define INF 0x0x7fffffff
+using namespace std;
+const int N = 0, mod = 1e9+7;
+
+void solve()
+{
+	int cnt = 0, n, m, x, y£¬ ans[11];
+	scanf("%d%d%d", &n, &x, &y);
+	for(int i = 0; i < n; i ++)
+	{
+		scanf("%d", &m);
+		for(int j = 0;  j < m; j ++)
+		{
+
+		}
+	}
+}
+
+int main()
+{
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    int T;
+	scanf("%d", &T);
+	while(T --) solve();
+    return 0;
+}
